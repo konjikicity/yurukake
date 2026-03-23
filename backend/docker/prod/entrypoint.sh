3eea38a2086949e44b696932c4846f3ea253d989
syu.onoda <s-onoda@creative-innovation.co.jp>
2026-03-23 14:32:51 +0900

    add: デプロイ調整

--- backend/docker/prod/entrypoint.sh
+++ backend/docker/prod/entrypoint.sh
@@ -6,5 +6,9 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+export PORT=${PORT:-8080}
+envsubst '${PORT}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default.tmp
+mv /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
+
 php-fpm -D
 nginx -g "daemon off;"
