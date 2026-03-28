e6ea1f4292d8a69704ff34d24ef402789e81ecab
syu.onoda <s-onoda@creative-innovation.co.jp>
2026-03-28 13:39:51 +0900

    add: デプロイ調整

--- backend/docker/prod/entrypoint.sh
+++ backend/docker/prod/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-php artisan migrate --force
+php artisan migrate --force || echo "Migration failed, continuing..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-export PORT=${PORT:-8080}
+export PORT=${PORT:-8000}
 envsubst '${PORT}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default.tmp
 mv /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 
