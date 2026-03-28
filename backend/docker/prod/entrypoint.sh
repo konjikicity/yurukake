269ccb20cd36facdb9761b8d2e7dee452a880b55
syu.onoda <s-onoda@creative-innovation.co.jp>
2026-03-28 13:51:16 +0900

    add: デプロイ調整

--- backend/docker/prod/entrypoint.sh
+++ backend/docker/prod/entrypoint.sh
@@ -10,5 +10,9 @@ export PORT=${PORT:-8000}
 envsubst '${PORT}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default.tmp
 mv /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 
+echo "Starting php-fpm..."
 php-fpm -D
+echo "Testing nginx config..."
+nginx -t
+echo "Starting nginx on port ${PORT}..."
 nginx -g "daemon off;"
